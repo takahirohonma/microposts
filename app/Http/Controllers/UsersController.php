f26b12732bd5a599e4c5e6789c4cfd67fffd524f
takahiro.honma <takahiro.ryuikihonma3776@gmail.com>
2020-11-14 02:01:08 +0000

    timeline

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -66,4 +66,13 @@ class UsersController extends Controller
             'users' => $followers,
         ]);
     }
+    
+    public function feed_microposts(){
+        // このユーザがフォロー中のユーザのidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        // このユーザのidもその配列に追加
+        $userIds[] = $this->id;
+        // それらのユーザが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
