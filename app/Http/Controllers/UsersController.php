fc84f3c3bb41ecc2eb3f537b082ae9b1d87327b4
takahiro.honma <takahiro.ryuikihonma3776@gmail.com>
2020-11-14 01:55:39 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,9 +13,57 @@ class UsersController extends Controller
         return view('users.index',['users'=> $users]);
     }
     
-    public function show($id){
+    public function show($id)
+    {
+        // idの値でユーザを検索して取得
         $user = User::findOrFail($id);
-        
-        return view('users.show', ['user' => $user]);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザの投稿一覧を作成日時の降順で取得
+        $microposts = $user->microposts()->orderBy('created_at', 'desc')->paginate(10);
+
+        // ユーザ詳細ビューでそれらを表示
+        return view('users.show', [
+            'user' => $user,
+            'microposts' => $microposts,
+        ]);
+    }
+    
+    public function followings($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロー一覧を取得
+        $followings = $user->followings()->paginate(10);
+
+        // フォロー一覧ビューでそれらを表示
+        return view('users.followings', [
+            'user' => $user,
+            'users' => $followings,
+        ]);
+    }
+    
+    public function followers($id)
+    {
+        // idの値でユーザを検索して取得
+        $user = User::findOrFail($id);
+
+        // 関係するモデルの件数をロード
+        $user->loadRelationshipCounts();
+
+        // ユーザのフォロワー一覧を取得
+        $followers = $user->followers()->paginate(10);
+
+        // フォロワー一覧ビューでそれらを表示
+        return view('users.followers', [
+            'user' => $user,
+            'users' => $followers,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'MicropostsController@index');    // 上書き
 
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
@@ -26,5 +24,13 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
 //ページ遷移したときに必ず認証を通す
 Route::group(['middleware' => ['auth']], function () {
-    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::group(['prefix' => 'users/{id}'], function () {
+        Route::post('follow', 'UserFollowController@store')->name('user.follow');
+        Route::delete('unfollow', 'UserFollowController@destroy')->name('user.unfollow');
+        Route::get('followings', 'UsersController@followings')->name('users.followings');
+        Route::get('followers', 'UsersController@followers')->name('users.followers');
+    });
+    
+Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
